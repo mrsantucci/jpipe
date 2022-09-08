3f5dc30f458769747afc295d94cb1463e9c28bdc
mrsantucci <litfuse.network@gmail.com>
2022-09-08 15:19:45 -0400

    Added auth and ui components

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import Amplify from 'aws-amplify';
 import config from '../src/aws-exports';
+import '@aws-amplify/ui-react/styles.css'
+
 Amplify.configure({
   ...config, ssr: true
 });
-
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
